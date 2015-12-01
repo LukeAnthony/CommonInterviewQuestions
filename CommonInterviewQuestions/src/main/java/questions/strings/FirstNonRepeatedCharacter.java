65e56290352dc14cea56c404ceb938fc27560fdb
00923103 <00923103@QSD083065.qvcdev.qvc.net>
2015-12-01 16:16:15 -0500

    Updated Readme

--- CommonInterviewQuestions/src/main/java/questions/strings/FirstNonRepeatedCharacter.java
+++ CommonInterviewQuestions/src/main/java/questions/strings/FirstNonRepeatedCharacter.java
@@ -1,5 +1,25 @@
 package questions.strings;
 
 public class FirstNonRepeatedCharacter {
+	
+	/**
+	 * Repository: Common Interview Questions
+	 * Category: Strings
+	 */
+	
+	/**
+	 * Problem: Find the first non-repeated character in a String
+	 * 
+	 * ex) In 'Luke', 'L' is the first non-repeated character.
+	 * 	   In 'Charcandrick', 'h' is the first non-repeated character.
+	 * 
+	 *  Complexity: O(N), must scan every character in the String. No need to scan more than a few times i.e. the solution should not be exponential
+	 */
+	public static void main(String[] args) {
+		
+		//
+		String evaluatedString = "Anthony";
+		
+	}
 
 }
