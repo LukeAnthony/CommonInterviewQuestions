03278ff3a7c00d9ab8f508f443d51a0a2d5e3829
00923103 <00923103@QSD083065.qvcdev.qvc.net>
2015-12-03 11:53:59 -0500

    Fully implemented my solution. Includes a test case and documentation

--- CommonInterviewQuestions/src/main/java/questions/strings/FirstNonRepeatedCharacter.java
+++ CommonInterviewQuestions/src/main/java/questions/strings/FirstNonRepeatedCharacter.java
@@ -1,5 +1,11 @@
 package questions.strings;
 
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class FirstNonRepeatedCharacter {
 	
 	/**
@@ -13,12 +19,41 @@ public class FirstNonRepeatedCharacter {
 	 * ex) In 'Luke', 'L' is the first non-repeated character.
 	 * 	   In 'Charcandrick', 'h' is the first non-repeated character.
 	 * 
-	 *  Complexity: O(N), must scan every character in the String. No need to scan more than a few times i.e. the solution should not be exponential
+	 * Solution: Create an empty linked hash map. Iterate through the string and check if each character is already in the map. If not, add it to the map with a value of 1. If so, increment the current
+	 * 	value by 1. Next, iterate through map until you find a value of 1. The corresponding key is the answer. If no values are equal to 1, then a  non-repeated character does not exist for that String.
+	 * 	
+	 * 
+	 * Complexity: O(N). Whole string will be scanned in all cases. Whole map will be scanned only if the last character added is the first non repeated character
+	 * 
+	 * Rational: I used a linked hash map because it keeps track of the order in which keys are entered into it. I also decided against using a third party String modifying library, like Apache's
+	 * 	'StringUtils' or 'Google Guava'. I wanted to use as much pure java as possible. 
 	 */
 	public static void main(String[] args) {
 		
-		//
-		String evaluatedString = "Anthony";
+		String evaluatee = "Charcandrick";
+		char[] charactersInEvaluatee = evaluatee.toLowerCase().toCharArray(); char answer = '\0';
+		Map<Character, Integer> characterFrequency = new LinkedHashMap<>();
+		
+		for(char character : charactersInEvaluatee){
+			if(characterFrequency.containsKey(character)){
+					characterFrequency.replace(character, characterFrequency.get(character) + 1);
+			}
+			else{
+				characterFrequency.put(character, 1);
+			}
+		}
+		
+		for(Character key : characterFrequency.keySet()){
+			if(characterFrequency.get(key) == 1){
+				answer = key;
+				break;
+			}
+		}
+		if(answer !='\0')		
+			System.out.println("The first non repeated character in the string " + evaluatee + " is " + answer);
+		else
+			System.out.println("There are no unique characters in this string");
+		
 		
 	}
 
--- README.md
+++ README.md
@@ -1,15 +1,3 @@
 # CommonInterviewQuestions
-This repository contains my solutions to a list of programming interview questions found here: 
+This repository contains my solutions to a list of programming interview questions found on r/cscareerquestions: 
 	https://www.reddit.com/r/cscareerquestions/comments/20ahfq/heres_a_pretty_big_list_of_programming_interview/
-
-I'll also use this readme to outline important programming concepts in my own words.
-
-Four Principles of Object Oriented Programming: 
-	
-	1) Encapsulation is the restriction of access to class data. In Java, for example, access modifiers can be used to hid ethe visibility of methods and/or 		instance variables. 
-	
-	2) Abstraction is a design technique that hides implementation. 
-	
-	3) Inheritance 
-	
-	4) 
