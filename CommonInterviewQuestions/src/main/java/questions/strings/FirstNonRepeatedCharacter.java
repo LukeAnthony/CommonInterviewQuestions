a6e21436d9331be83977eb2aadc4c07f0ef37c0d
00923103 <00923103@QSD083065.qvcdev.qvc.net>
2015-12-03 13:51:21 -0500

    Code is now object-oriented and does not consider a space to be a character. Also added a StringReversal class

--- CommonInterviewQuestions/src/main/java/questions/strings/FirstNonRepeatedCharacter.java
+++ CommonInterviewQuestions/src/main/java/questions/strings/FirstNonRepeatedCharacter.java
@@ -8,9 +8,6 @@ public class FirstNonRepeatedCharacter {
 	/**
 	 * Repository: Common Interview Questions
 	 * Category: Strings
-	 */
-	
-	/**
 	 * Problem: Find the first non-repeated character in a String
 	 * 
 	 * ex) In 'Luke', 'L' is the first non-repeated character.
@@ -18,20 +15,25 @@ public class FirstNonRepeatedCharacter {
 	 * 
 	 * Solution: Create an empty linked hash map. Iterate through the string and check if each character is already in the map. If not, add it to the map with a value of 1. If so, increment the current
 	 * 	value by 1. Next, iterate through map until you find a value of 1. The corresponding key is the answer. If no values are equal to 1, then a  non-repeated character does not exist for that String.
-	 * 	
 	 * 
 	 * Complexity: O(N). Whole string will be scanned in all cases. Whole map will be scanned only if the last character added is the first non repeated character
 	 * 
 	 * Rational: I used a linked hash map because it keeps track of the order in which keys are entered into it. I also decided against using a third party String modifying library, like Apache's
 	 * 	'StringUtils' or 'Google Guava'. I wanted to use as much pure java as possible. 
 	 */
-	public static void main(String[] args) {
-		
-		String evaluatee = "Charcandrick";
-		char[] charactersInEvaluatee = evaluatee.toLowerCase().toCharArray(); char answer = '\0';
-		Map<Character, Integer> characterFrequency = new LinkedHashMap<>();
+	private String evaluatee;
+	private char[] charactersInEvaluatee;
+	private Map<Character, Integer> characterFrequency = new LinkedHashMap<>();
+	
+	public FirstNonRepeatedCharacter(String evaluatee){
+		this.evaluatee = evaluatee;
+		charactersInEvaluatee = evaluatee.toLowerCase().toCharArray();
+	}
+	
+	private void evaluateCharacterFrequencies(){
 		
 		for(char character : charactersInEvaluatee){
+		  if(character != ' ')
 			if(characterFrequency.containsKey(character)){
 					characterFrequency.replace(character, characterFrequency.get(character) + 1);
 			}
@@ -39,19 +41,25 @@ public class FirstNonRepeatedCharacter {
 				characterFrequency.put(character, 1);
 			}
 		}
+	}
+	
+	private String findFirstRepeatedCharacter(){
 		
 		for(Character key : characterFrequency.keySet()){
 			if(characterFrequency.get(key) == 1){
-				answer = key;
-				break;
+				return( "The first non repeated character in the string " + evaluatee + " is " + key);
 			}
 		}
-		if(answer !='\0')		
-			System.out.println("The first non repeated character in the string " + evaluatee + " is " + answer);
-		else
-			System.out.println("There are no unique characters in this string");
 		
+		return ("There are no unique characters in this " + evaluatee);
+	}
+	
+	public static void main(String[] args) {
 		
+		FirstNonRepeatedCharacter test = new FirstNonRepeatedCharacter("The Dear Hunter");
+		test.evaluateCharacterFrequencies();
+		System.out.println(test.findFirstRepeatedCharacter());
+			
 	}
 
 }
---
+++ CommonInterviewQuestions/src/main/java/questions/strings/StringReversal.java
@@ -0,0 +1,16 @@
+package questions.strings;
+
+public class StringReversal {
+
+	/**
+	 * Problem: Reverse a string iteratively and recursively
+	 * 
+	 * Ex) 'Luke' --> 'ekuL', 'Andrew' --> 'werdnA'
+	 * 
+	 *  
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
+}
