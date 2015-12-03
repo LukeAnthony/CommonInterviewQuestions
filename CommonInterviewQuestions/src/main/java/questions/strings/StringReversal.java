17f3e768c2228b41d8650ff125a7b0b58800f671
00923103 <00923103@QSD083065.qvcdev.qvc.net>
2015-12-03 15:49:19 -0500

    last minute changes before leaving work

--- CommonInterviewQuestions/src/main/java/questions/strings/StringReversal.java
+++ CommonInterviewQuestions/src/main/java/questions/strings/StringReversal.java
@@ -8,9 +8,32 @@ public class StringReversal {
 	 * Ex) 'Luke' --> 'ekuL', 'Andrew' --> 'werdnA'
 	 * 
 	 *  
+	 *  Complexity: O(N). Only need to iterate through the String's characters once
 	 */
+	private String evaluatee;
+	
+	public StringReversal(String evaluatee){
+		this.evaluatee = evaluatee;
+	}
+	
+	public StringBuilder iterativeReversal(){
+		
+		StringBuilder reversedEvaluatee = new StringBuilder();
+		int evaluateeLength = evaluatee.length();
+		for(int i = evaluateeLength-1; i > -1; i--){
+			reversedEvaluatee.append(evaluatee.charAt(i));
+		}
+		return reversedEvaluatee;
+	}
+	
+	public String recursiveReversal(int i){
+		
+		return recursiveReversal(i-1);
+	}
 	
 	public static void main(String[] args) {
-		
+		StringReversal test = new StringReversal("Andrew");
+		System.out.println(test.iterativeReversal());
+
 	}
 }
